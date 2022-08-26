e3a0986b55a50f90894469bd8c4f02a372d51640
pwaclawiak <p.waclawiak@samorzad.p.lodz.pl>
2022-08-26 06:49:51 +0200

    Separated out a state class from cubit

--- lib/bloc/counter_cubit.dart
+++ lib/bloc/counter_cubit.dart
@@ -1,18 +1,19 @@
 import 'package:bloc/bloc.dart';
 
-class CounterCubit extends Cubit<int> {
-  int incrementStep;
-  int decrementStep;
+class CounterState {
+  int currentValue = 0;
+  int incrementStep = 1;
+  int decrementStep = 1;
 
-  CounterCubit(this.incrementStep, int startValue, int decrementStep)
-      : super(startValue) {
-    try {
-      this.decrementStep = decrementStep;
-    } catch (exception) {
-      decrementStep = 1;
-    }
-  }
+  CounterState(this.currentValue, this.incrementStep, this.decrementStep);
+}
+
+class CounterCubit extends Cubit<CounterState> {
+  CounterCubit(int startValue, int incrementStep, int decrementStep)
+      : super(CounterState(startValue, incrementStep, decrementStep));
 
   void increment() => emit(state + incrementStep);
   void decrement() => emit(state - decrementStep);
+  void increase(int value) => emit(state + value);
+  void decrease(int value) => emit(state - value);
 }
